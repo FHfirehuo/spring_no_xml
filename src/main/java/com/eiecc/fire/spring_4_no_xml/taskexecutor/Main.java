ce726c142423d2d3421255a533163a2c70d67601
fire <gegeaininiaige@outlook.com>
2016-11-29 17:59:10 +0800

    多线程

---
+++ src/main/java/com/eiecc/fire/spring_4_no_xml/taskexecutor/Main.java
@@ -0,0 +1,20 @@
+package com.eiecc.fire.spring_4_no_xml.taskexecutor;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class Main {
+
+	public static void main(String[] args) {
+		AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(TaskExecutorConfig.class);
+
+
+		AsyncTaskService asyncTaskService = context.getBean(AsyncTaskService.class);
+		for(int i = 0 ; i < 10 ;i++){
+			asyncTaskService.executeAsyncTask(i);
+			asyncTaskService.executeAsyncTaskPlus(i);
+		}
+
+		context.close();
+	}
+
+}
